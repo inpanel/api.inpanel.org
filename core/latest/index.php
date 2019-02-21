4bf5b3f05c1ca889824311500bd63a4d3d2e1e8d
唐钰豆豆 <doudoudzj@gmail.com>
2019-02-22 00:15:55 +0800

    Update index.php

--- core/latest/index.php
+++ core/latest/index.php
@@ -7,6 +7,6 @@ return array(
     'version' => APP_VERSION,
     'build' => APP_BUILD,
     'releasetime' => APP_RELEASE,
-    'download' => 'http://api.intranet.pub/?s=download&version=' . APP_VERSION . '&build=' . APP_BUILD,
+    'download' => 'http://api.inpanel.org/?s=download&version=' . APP_VERSION . '&build=' . APP_BUILD,
     'changelog' => 'https://inpanel.org/changelog.html',
 );
