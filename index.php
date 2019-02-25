911e3a96dd12ca0e040f53ddfe83ce80779eee44
唐钰豆豆 <doudoudzj@gmail.com>
2019-02-25 14:27:23 +0800

    Release version InPanel v1.1.1 b18

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
+// error_reporting(E_ALL ^ E_NOTICE);
 define('APP_API', true);
 define('ROOT', dirname(__FILE__));
 define('APP_NAME', 'InPanel');
 define('APP_SITE', 'https://inpanel.org');
 define('APP_VERSION', '1.1.1');
-define('APP_BUILD', '17');
-define('APP_RELEASE', '2018-12-20 18:57:43 CST');
+define('APP_BUILD', '18');
+define('APP_RELEASE', '2019-02-23 14:13:00 CST');
 define('APP_DEBUG', 0);
 header("X-Powered-By: InPanel");
 header("Copyright: InPanel");
-// error_reporting(E_ALL ^ E_NOTICE);
 date_default_timezone_set('Asia/Shanghai');
 require_once ROOT . '/core/Request.php';
 require_once ROOT . '/core/Response.php';
