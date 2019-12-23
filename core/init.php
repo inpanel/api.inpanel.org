9cbe0b9d745cf9483fb65adb53191e287cdfb4f8
唐钰豆豆 <doudoudzj@qq.com>
2019-12-24 06:14:38 +0800

    Update init.php

--- core/init.php
+++ core/init.php
@@ -6,8 +6,8 @@ if (!defined('APP_API')) {
 define('APP_NAME', 'InPanel');
 define('APP_SITE', 'https://inpanel.org');
 define('APP_VERSION', '1.1.1');
-define('APP_BUILD', '20');
-define('APP_RELEASE', '2019-08-08 16:40:40 GMT+0800');
+define('APP_BUILD', '21');
+define('APP_RELEASE', '2019-12-24 05:58:01 GMT+0800');
 define('APP_DEBUG', 0);
 header("X-Powered-By: INPANEL");
 header("Copyright: INPANEL");
